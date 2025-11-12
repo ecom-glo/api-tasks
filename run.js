1625d157929814f0ea3a4b050c3e35f133e02a53
ecom-glo <glo@ecomsystems.com>
2025-11-12 15:01:31 -0500

    updated run.js

--- run.js
+++ run.js
@@ -1,18 +1,25 @@
 const axios = require('axios');
 
+// Your Domo API credentials
+const clientId = process.env.DOMO_CLIENT_ID;
+const clientSecret = process.env.DOMO_CLIENT_SECRET;
+
+// Encode as Base64
+const token = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+
 let config = {
   method: 'get',
   maxBodyLength: Infinity,
   url: 'https://api.domo.com/oauth/token?grant_type=client_credentials',
   headers: { 
-    'Authorization': 'Basic ZDY4Y2FjNTUtY2UzMy00NDQ4LTg1OWEtMTMwNjYxOTc2NzQ4OmU2NDY4NGMxMDdlZDc4ODBlYWFlY2NhMzJlMzVkZThjOThkMTMxZmI5NGJjNWZkYTc3MmE2NGM3MDE0YjVlYWE='
+    'Authorization': `Basic ${token}`
   }
 };
 
 axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
